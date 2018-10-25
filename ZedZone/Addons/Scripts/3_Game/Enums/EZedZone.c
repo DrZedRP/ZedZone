394e7f5b8ea1c614b2d393f9ef685cef4484723f
DrZedRP <43376065+DrZedRP@users.noreply.github.com>
2018-10-25 09:05:01 +0200

    removed rpc enum

--- ZedZone/Addons/Scripts/3_Game/Enums/EZedZone.c
+++ ZedZone/Addons/Scripts/3_Game/Enums/EZedZone.c
@@ -1,31 +1,13 @@
-enum ZZeRPCs
-{
-	// Z = 90
-	RPC_ZZ_ZONE_STAGE = 9090,
-
-
-}
-
-enum ZZeZoneStage  {
-	NONE = 0,
-	OUTER,
-	INNER,
-	CORE
-}
+// Z = 90 ZZ = 9090
 
 enum ZZeModifiers 
 {	
-	// Z = 90
+
 	MDF_DEBUG = 9090,
 	MDF_ZONE_TRACKER,
 	MDF_ZONE_DEBUFF, //DEBUFF
 	MDF_ZONE_SICKNESS,
 	MDF_ZONE_DAMAGE,
-	MDF_ZONE_DAMAGE_OUTER,
-	MDF_ZONE_DAMAGE_INNER,
-	MDF_ZONE_DAMAGE_CORE
-	
-	
 
 }
 
@@ -39,16 +21,20 @@ enum ZZeStateIDs {
 
 
 enum eZones {
+
 	ZONE_NONE = 0,
 	ZONE_GAS_VERYLOW,
 	ZONE_GAS_LOW,
 	ZONE_GAS_MEDIUM,
 	ZONE_GAS_HIGH,
 	ZONE_GAS_VERYHIGH
+
 };
 
 enum eZoneTypes {
+
 	TYPE_NONE = 0,
 	TYPE_RADIATION,
 	TYPE_GAS
+	
 }
